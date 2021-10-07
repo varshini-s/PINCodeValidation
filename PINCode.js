9d6aeaa4021db951cdf0700f87d36684975954b1
varshini <dreamvarshini@gmail.com>
2021-10-07 20:52:10 +0530

    ADDED: pincode class to validate 6 digit pincode

---
+++ PINCode.js
@@ -0,0 +1,25 @@
+const PIN_CODE_PATTERN=RegExp("[0-9]{6}");
+
+class PINCode
+{
+
+    vaildatePinCode(pincodeValue) 
+    {
+        if (PIN_CODE_PATTERN.test(pincodeValue))
+        {
+            console.log(("Pin Code "+pincodeValue+" is  valid"));
+        }
+        else
+        {
+            console.log(("Pin Code "+pincodeValue+" is  not valid"));
+
+            
+        }
+
+    }
+
+    
+}
+
+let pincode=new PINCode();
+pincode.vaildatePinCode("400088")
